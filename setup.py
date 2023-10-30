1bfe861d0ea72ab837843fa236b21248cb15fd77
meanieminnie <mrinalini.nayak12@journalism.cuny.edu>
2023-10-30 15:01:56 -0400

    fixed another booboo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='testFuncMN-package-demo', ##name of repo enclosing folder (pip-package-demo)
+    name='testFuncMN', ##name of repo enclosing folder (pip-package-demo)
     version='0.0.2',
     author='Mrinalini Nayak', ## your name
     author_email='mnayak20.mn@gmail.com', ## your email
