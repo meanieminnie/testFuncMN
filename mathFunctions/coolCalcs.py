605b2322f6c0e114e86679a887551563810fb3e3
meanieminnie <mrinalini.nayak12@journalism.cuny.edu>
2023-10-30 14:35:05 -0400

    added ds store to gitignore file

---
+++ mathFunctions/coolCalcs.py
@@ -0,0 +1,13 @@
+## function to add two numbers
+def addNumbers(number1, number2):
+    '''provise 2 numbers as parameteres. 
+    function returns total
+    '''
+    return number1 + number2
+    
+## function to subtract two numbers
+def subNumbers(number1, number2):
+    '''provide 2 numbers as parameters. 
+    function subtracts 2 from 1, returns total.
+    '''
+return number1-number2
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pip-package-demo', ##name of repo enclosing folder (pip-package-demo)
+    version='0.0.2',
+    author='Sandeep Junnarkar', ## your name
+    author_email='sjnews@gmail.com', ## your email
+    description='Demo on pip package creation', ## description of package
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/sandeepmj/pip-package-demo/tree/main',# url to repo
+    project_urls = {
+        "Bug Tracker": "https://github.com/sandeepmj/pip-package-demo/issues"
+    }, ## url to issues page on your repo
+    license='MIT',
+    packages=['demo'], ## name of folder that holds the functions
+    install_requires=[], ## names of packages required to run your functions
+)
